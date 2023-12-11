93a420c169d7f8bd0f34e1fc671fda144d7bc178
sxBUTCHxs <leonschoenberg@gmx.de>
2023-12-11 20:20:21 +0100

    fix bug.

--- PiHoleController/views.py
+++ PiHoleController/views.py
@@ -12,11 +12,19 @@ class PiHoleView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = dict()
-        subprocess.run()
-        if "inactive" in subprocess.run(['sudo', 'systemctl', 'status', 'pihole-FTL'], check=True, capture_output=True, text=True):
+
+        # Befehl zum Überprüfen des Status von pihole-FTL
+        command = ['sudo', 'systemctl', 'status', 'pihole-FTL']
+
+        # Führen Sie den Befehl aus und erfassen Sie die Ausgabe
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+
+        # Überprüfen Sie, ob pihole-FTL aktiv ist
+        if "inactive" in result.stdout:
             context["active"] = False
         else:
             context["active"] = True
+
         return context
 
 
