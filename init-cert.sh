9ecffb631d5b3192f715ec543aa4e3fe78d285ab
Leon <mail@leon-schoenberg.de>
2025-07-19 09:11:14 +0200

    better ci/cd fix problem 6

--- init-cert.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/sh
-
-certbot certonly --webroot \
-  --webroot-path=/var/www/certbot \
-  --email "$CERTBOT_EMAIL" \
-  --agree-tos \
-  --no-eff-email \
-  $(echo "$CERTBOT_DOMAINS" | xargs -n1 echo -d | tr '\n' ' ')
